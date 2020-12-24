e1034b95e0d520a49c4c1d479496697bed1ad3ef
Camila Paschini <camila.paschini@gmail.com>
2020-12-24 18:40:08 +0100

    using a DataFetcher to deal with partial results

--- src/main/java/com/titanstate/learn/graphql/resolver/bank/ClientResolver.java
+++ src/main/java/com/titanstate/learn/graphql/resolver/bank/ClientResolver.java
@@ -3,6 +3,8 @@ package com.titanstate.learn.graphql.resolver.bank;
 import com.titanstate.learn.graphql.domain.bank.BankAccount;
 import com.titanstate.learn.graphql.domain.bank.Client;
 import graphql.GraphQLException;
+import graphql.execution.DataFetcherResult;
+import graphql.kickstart.execution.error.GenericGraphQLError;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -12,7 +14,7 @@ import java.util.UUID;
 @Slf4j
 @Component
 public class ClientResolver implements GraphQLResolver<BankAccount> {
-  public Client client(BankAccount bankAccount) {
+  public DataFetcherResult<Client> client(BankAccount bankAccount) {
     log.info("Requesting client data for bank account id: {}", bankAccount.getId());
 
     // throw new GraphQLException("Client unavailable"); // internal server errors get hidden!
@@ -23,10 +25,15 @@ public class ClientResolver implements GraphQLResolver<BankAccount> {
      * 2: call another graphql server
      * 3: call service that returns partial responses
      */
-    return Client.builder()
-        .id(UUID.randomUUID())
-        .firstName("Camila")
-        .lastName("Paschini")
+
+    return DataFetcherResult.<Client>newResult()
+        .data(Client.builder()
+            .id(UUID.randomUUID())
+            .firstName("Camila")
+            .lastName("Paschini")
+            .build())
+        .error(new GenericGraphQLError("Could not get client id!"))
         .build();
+    // no throwing exception needed this way
   }
 }
